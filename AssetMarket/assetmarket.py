96eb5d28e4048f8a6f508e04417bb987bdc84914
小川慶将 <yshms_o@yahoo.co.jp>
2015-01-26 13:08:27 +0900

    no message

---
+++ AssetMarket/assetmarket.py
@@ -0,0 +1,178 @@
+# coding: UTF-8
+from willow import *
+from wifunc_v1 import *
+import numpy as np
+import random
+from pandas import *
+import matplotlib.pyplot as plt
+
+
+def session(me):
+    # 為替レートの推移を何回するか変更可能
+    ts = 50
+    if me == 0:
+        add("<h1>Asset Market : Monitor</h1>")
+        # 人数登録
+        numset()
+        take({"client": me})
+        plnum = get_num()
+        numset_end(plnum)
+        # 実験説明開始(a)
+        wiput(plnum, {"tag": "a"})
+        wait(1)
+        # 実験内容把握(b)
+        witake(plnum, {"tag": "b"})
+        waithide(1)
+        # 実験開始確認
+        start()
+        take({"client": me})
+        hide("#start")
+        # 実験開始(c)
+        add(open("assetmarket_moni.html"))
+        wiput(plnum, {"tag": "c"})
+        # 変数・関数の定義
+        buyprice = 0
+        sellprice = 1000000
+        buynum = 0
+        sellnum = 0
+        time = 10
+        dividend_list = [0, 8, 28, 60]
+        def timer():
+            put({"id": "d", "tag": 1})
+        def ping():
+            put({"id": "d", "tag": 2})
+        for i in range(15):
+            background(ping, time)
+            for j in range(time):
+                background(timer, j)
+            while True:
+                # 注文を受け取る(d)
+                msg = take({"id": "d"})
+                if msg["tag"] == 1:
+                    wiput(plnum, {"id": "e", "tag": 2})
+                elif msg["tag"] == 2:
+                    break
+                elif msg["type"] == "buy":
+                    if buyprice < msg["value"]:
+                        buyprice = msg["value"]
+                        buynum = msg["client"]
+                        let(msg["value"], "#buyprice")
+                        let(msg["client"], "#buynum")
+                    # 板情報更新(e)
+                    wiput(plnum, {"id": "e", "tag": 3, "type": "buy",
+                    "buyprice": buyprice, "buynum": buynum})
+                elif msg["type"] == "sell":
+                    if sellprice > msg["value"]:
+                        sellprice = msg["value"]
+                        sellnum = msg["client"]
+                        let(msg["value"], "#sellprice")
+                        let(msg["client"], "#sellnum")
+                    # 板情報更新(e)
+                    wiput(plnum, {"id": "e", "tag": 3, "type": "sell",
+                    "sellprice": sellprice, "sellnum": sellnum})
+                if buyprice > sellprice:
+                    wiput(plnum, {"id": "e", "tag": 4, "buyprice": buyprice,
+                    "buynum": buynum, "sellprice": sellprice,
+                    "sellnum": sellnum})
+                    logprice = (buyprice + sellprice) * 0.5
+                    exbond = (15 - i) * 24
+                    log("期待収益と約定価格", exbond, logprice)
+                    let("(入札なし)", "#buyprice")
+                    let("(入札なし)", "#buynum")
+                    let("(入札なし)", "#sellprice")
+                    let("(入札なし)", "#sellnum")
+                    buyprice = 0
+                    sellprice = 1000000
+                    buynum = 0
+                    sellnum = 0
+            # 配当の決定(e)
+            dividend = random.choice(dividend_list)
+            wiput(plnum, {"id": "e", "tag": 5, "div": dividend})
+        add("<p>これで実験を終了します。</p>")
+
+    else:
+        add("<h1>Asset Market : Client_No.%s</h1>" % me)
+        wait(1)
+        # 実験説明開始(a)
+        take({"tag": "a"})
+        waithide(1)
+        # 実験内容把握(b)
+        nameset()
+        take({"client": me})
+        myname = get_name()
+        hide("#nameset")
+        put({"tag": "b"})
+        wait(2)
+        # 実験開始(c)
+        take({"tag": "c"})
+        waithide(2)
+        add(open("assetmarket_sub.html"))
+        # 変数・関数の定義
+        capital = 100
+        bond = 10
+        buyprice = 0
+        sellprice = 1000000
+        buynum = 0
+        sellnum = 0
+        time = 10
+        let(capital, "#capital")
+        let(bond, "#bond")
+        for i in range(15):
+            counter = time
+            let(counter, "#time")
+            let(i+1, "#number")
+            while True:
+                # 板情報更新、もしくは注文受け取り(e)
+                # 他の被験者も注文を受け取れてしまう。
+                msg = take({"id": "e", "client": me})
+                # タイマー更新
+                if msg["tag"] == 2:
+                    counter -= 1
+                    let(counter, "#time")
+                # 板情報更新
+                elif msg["tag"] == 3:
+                    if msg["type"] == "buy":
+                        let(msg["buyprice"], "#buyprice")
+                        let(msg["buynum"], "#buynum")
+                    if msg["type"] == "sell":
+                        let(msg["sellprice"], "#sellprice")
+                        let(msg["sellnum"], "#sellnum")
+                # 取引成立
+                elif msg["tag"] == 4:
+                    if msg["buynum"] == me:
+                        capital -= msg["buyprice"]
+                        bond += 1
+                    if msg["sellnum"] == me:
+                        capital += msg["sellprice"]
+                        bond -= 1
+                    add("プレイヤー%sとプレイヤー%sの間で取引が成立しました。<br />" %
+                    (msg["buynum"], msg["sellnum"]), "#info")
+                    let("(入札なし)", "#buyprice")
+                    let("(入札なし)", "#buynum")
+                    let("(入札なし)", "#sellprice")
+                    let("(入札なし)", "#sellnum")
+                    let(capital, "#capital")
+                    let(bond, "#bond")
+                # 配当の受け渡し
+                elif msg["tag"] == 5:
+                    break
+                # 注文(d)
+                else:
+                    if (peek("#ask") != "") and (peek("#bid") != ""):
+                        add("買い売りどちらの欄にも値が記入されています。<br />",
+                        "#info")
+                    elif peek("#ask") != "":
+                        put({"id": "d", "type": "buy", "value": int(peek("#ask")),
+                        "client": me, "tag": 0})
+                    elif peek("#bid") != "":
+                        put({"id": "d", "type": "sell", "value": int(peek("#bid")),
+                        "client": me, "tag": 0})
+            dividend = msg["div"]
+            capital += dividend * bond
+            let(capital, "#capital")
+            let(bond, "#bond")
+            add("%s期目の配当額は証券1枚あたり%s円でした。<br />" % (i+1, dividend), "#info")
+        add("<p>これで実験を終了します。</p>", "#info")
+        log(me, myname, capital)
+
+run(session)
---
+++ AssetMarket/wifunc_v1.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+from willow.willow import *
+
+def numset():
+    add("<p id='numset'>参加人数を入力してください。<br />"
+    "<input id='plnum' type='text' />"
+    "<input id='go' type='submit'></p>")
+
+
+def get_num():
+    return int(peek("#plnum"))
+
+
+def nameset():
+    add("<p id='nameset'>実験の内容について理解し、"
+    "準備が整いましたら名前を入力してボタンを押してください。<br />"
+    "<input id='myname' type='text' />"
+    "<input id='go' type='submit'></p>")
+
+
+def get_name():
+    return peek("#myname")
+
+
+def numset_end(plnum):
+    hide("#numset")
+    add("<p id='numset_end'>実験人数：<span id='num'></span>人</p>")
+    add(plnum, "#num")
+
+
+def table_css():
+    add("<style type='text/css'>table {border-collapse: collapse;}</style>")
+    add("<style type='text/css'>td {border: solid 1px;padding: 0.5em;}</style>")
+
+
+def wait(a, b=0):
+    add("<p id='wait%s'>しばらくお待ちください。</p>" % str(a+b*10000))
+
+
+def waithide(a, b=0):
+    hide("#wait%s" % str(a+b*10000))
+
+
+def wiput(a, b):
+    for i in range(a):
+        b.update({"client": i+1})
+        put(b)
+
+def witake(a, b):
+    for i in range(a):
+        take(b)
+
+def ready():
+    add("<p id='ready'>実験の内容について理解し、"
+    "準備が整いましたらボタンを押してください。<br />"
+    "<input id='go' type='submit'></p>")
+
+
+def start():
+    add("<p id='start'>被験者の準備が整いました。<br />"
+    "ボタンを押すと実験が始まります。<br />"
+    "<input id='go' type='submit'></p>")
+
+
+def DApriceset(a):
+    # a = plnum
+    add("<p id='priceset'>私的価値を入力してください。<br />"
+    "買い手<br /><span id='buypr'></span>"
+    "売り手<br /><span id='sellpr'></span>"
+    "<input id='go' type='submit'></p>")
+    if a % 2 == 0:
+        for i in range(a/2):
+            add("<input id='pr%s' type='text' /><br />" % i, "#buypr")
+        for i in range(a/2, a):
+            add("<input id='pr%s' type='text' /><br />" % i, "#sellpr")
+    else:
+        for i in range((a-1)/2):
+            add("<input id='pr%s' type='text' /><br />" % i, "#buypr")
+        for i in range((a-1)/2, a):
+            add("<input id='pr%s' type='text' /><br />" % i, "#sellpr")
+
+
+def DApriceset_end(a, b, c):
+    # a = plnum
+    # b = buylist
+    # c = selllist
+    hide("priceset")
+    add("<p id='priceset_end'>"
+    "買い手<br /><span id='buypr_end'></span><br />"
+    "売り手<br /><span id='sellpr_end'></span></p>")
+    if a % 2 == 0:
+        for i in range(a/2):
+            b.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#buypr_end")
+        for i in range(a/2, a):
+            c.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#sellpr_end")
+    else:
+        for i in range((a-1)/2):
+            b.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#buypr_end")
+        for i in range((a-1)/2, a):
+            c.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#sellpr_end")
+
+
+def AFCpriceset(a):
+    # a = plnum
+    add("<p id='priceset'>私的価値を入力してください。<br />"
+    "I-type1<br /><span id='ipr1'></span><br />"
+    "I-type2<br /><span id='ipr2'></span><br />"
+    "J-type<br /><span id='jpr'></span><br />"
+    "<input id='go' type='submit'></p>")
+    if a % 3 == 0:
+        for i in range(a/3):
+            add("<input id='pr%s' type='text' /><br />" % i, "#ipr1")
+        for i in range(a/3, 2*a/3):
+            add("<input id='pr%s' type='text' /><br />" % i, "#ipr2")
+        for i in range(2*a/3, a):
+            add("<input id='pr%s' type='text' /><br />" % i, "#jpr")
+    elif a % 3 == 1:
+        for i in range((a-1)/3 + 1):
+            add("<input id='pr%s' type='text' /><br />" % i, "#ipr1")
+        for i in range((a-1)/3 + 1, 2*(a-1)/3 + 1):
+            add("<input id='pr%s' type='text' /><br />" % i, "#ipr2")
+        for i in range(2*(a-1)/3 + 1, a):
+            add("<input id='pr%s' type='text' /><br />" % i, "#jpr")
+    else:
+        for i in range((a-2)/3 + 1):
+            add("<input id='pr%s' type='text' /><br />" % i, "#ipr1")
+        for i in range((a-2)/3 + 1, 2*(a-2)/3 + 2):
+            add("<input id='pr%s' type='text' /><br />" % i, "#ipr2")
+        for i in range(2*(a-2)/3 + 2, a):
+            add("<input id='pr%s' type='text' /><br />" % i, "#jpr")
+
+
+def AFCpriceset_end(a, b, c, d):
+    # a = plnum
+    # b = itype1list
+    # c = itype2list
+    # d = jtypelist
+    hide("#priceset")
+    add("<p id='priceset_end'>"
+    "I-type1<br /><span id='ipr1_end'></span><br />"
+    "I-type2<br /><span id='ipr2_end'></span><br />"
+    "J-type<br /><span id='jpr_end'></span></p>")
+    if a % 3 == 0:
+        for i in range(a/3):
+            b.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#ipr1_end")
+        for i in range(a/3, 2*a/3):
+            c.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#ipr2_end")
+        for i in range(2*a/3, a):
+            d.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#jpr_end")
+    elif a % 3 == 1:
+        for i in range((a-1)/3 + 1):
+            b.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#ipr1_end")
+        for i in range((a-1)/3 + 1, 2*(a-1)/3 + 1):
+            c.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#ipr2_end")
+        for i in range(2*(a-1)/3 + 1, a):
+            d.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#jpr_end")
+    else:
+        for i in range((a-2)/3 + 1):
+            b.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#ipr1_end")
+        for i in range((a-2)/3 + 1, 2*(a-2)/3 + 2):
+            c.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#ipr2_end")
+        for i in range(2*(a-2)/3 + 2, a):
+            d.append(int(peek("#pr%s" % i)))
+            add("%s円  " % int(peek("#pr%s" % i)), "#jpr_end")
